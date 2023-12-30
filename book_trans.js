985d64c4246a8a7374cbbcc90cf77b7b78d3d7b7
gitxandert <alexandertoth96@yahoo.com>
2023-12-29 21:03:41 -0500

    Add more id setting functions

--- book_trans.js
+++ book_trans.js
@@ -2,3 +2,13 @@ function trans(){
     let book=document.getElementById('book');
     book.id='book_trans';
 }
+
+function vid_trans(){
+    let video=document.querySelector('#trailer');
+    video.id='video_trans';
+}
+
+function amaz_trans(){
+    let link=document.querySelector('a');
+    link.id='video_trans';
+}
